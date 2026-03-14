3dd66149a90ae609f47f7b365a89eceb17db32b2
SyedMash <mashrtx7@gmail.com>
2026-03-15 03:33:32 +0500

    feat: add ingest and retrieval modules for document processing and storage

--- app/rag/ingest.py
+++ app/rag/ingest.py
@@ -1,3 +1,4 @@
+import hashlib
 from app.core.config import get_settings, Settings
 
 from qdrant_client import QdrantClient
@@ -12,6 +13,7 @@ from qdrant_client.http.models import (
     PayloadSchemaType,
 )
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
 
 settings: Settings = get_settings()
 
@@ -59,3 +61,55 @@ def get_vector_store(client: QdrantClient) -> QdrantVectorStore:
     )
 
     return vector_store
+
+
+def create_chunk_id(url: str, chunk_index: int) -> str:
+    # Create a unique identifier for the chunk using a hash of the URL and chunk index
+    unique_string = f"{url}_{chunk_index}"
+    return hashlib.sha256(unique_string.encode()).hexdigest()
+
+
+def load_from_sitemap(sitemap_url: str) -> list[Document]:
+    loader = SitemapLoader(web_path=sitemap_url)
+    loader.requests_per_second = 2
+    return loader.load()
+
+
+def load_from_urls(urls: list[str]) -> list[Document]:
+    loader = WebBaseLoader(web_paths=urls)
+    return loader.load()
+
+
+def inngest(sitemap_url: str | None, urls: list[str] | None):
+    if sitemap_url:
+        docs = load_from_sitemap(sitemap_url)
+    elif urls:
+        docs = load_from_urls(urls)
+    else:
+        raise ValueError("Sitemap url or URL not provided")
+
+    for doc in docs:
+        url = doc.metadata.get("source", "")
+        parts = url.rstrip("/").split("/")
+        doc.metadata["category"] = parts[-2] if len(parts) >= 2 else "general"
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=600,
+        chunk_overlap=200,
+        len_func=len,
+        separators=["\n\n", "\n", " ", ""],
+    )
+    chunks = splitter.split_documents(docs)
+
+    for i, chunk in enumerate(chunks):
+        chunk.metadata["chunk_id"] = create_chunk_id(
+            url=chunk.metadata.get("source", ""), chunk_index=i
+        )
+        chunk.metadata["chunk_index"] = i
+
+    client = QdrantClient(url=settings.qdrant_url)
+    create_collection(client)
+    store = get_vector_store(client)
+    store.add_documents(chunks)
+
+    return len(chunks)
